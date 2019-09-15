1333ac6f9b7216f25aa02d8887029923b8eb3ae5
Lloyd Brookes <lloyd.brookes@gmail.com>
2019-09-15 02:37:25 +0100

    update deps, dates and travis

--- index.js
+++ index.js
@@ -1,38 +1,36 @@
 'use strict'
-var os = require('os')
 
 /**
-* Cross-platform home directory retriever, tested on Windows XP and above, Mac OSX and Linux.
-*
-* With node versions 2.3.0 (iojs) or higher, the built-in [`os.homedir`](https://nodejs.org/api/os.html#os_os_homedir) method is used.
-*
-* @module home-path
-* @example
-* var getHomePath = require('home-path')
-*/
-module.exports = os.homedir ? os.homedir : getHomePath
+ * Cross-platform home directory retriever, tested on Windows XP and above, Mac OSX and Linux.
+ *
+ * With node versions 2.3.0 (iojs) or higher, the built-in [`os.homedir`](https://nodejs.org/api/os.html#os_os_homedir) method is used.
+ *
+ * @module home-path
+ * @example
+ * var getHomePath = require('home-path')
+ */
 
 /**
-* @alias module:home-path
-* @example
-* Mac OSX
-* ```js
-* > getHomePath()
-* '/Users/Lloyd'
-* ```
-*
-* Ubuntu Linux
-* ```js
-* > getHomePath()
-* '/home/lloyd'
-* ```
-*
-* Windows 8.1
-* ```js
-* > getHomePath()
-* 'C:\\Users\\Lloyd'
-* ```
-*/
+ * @alias module:home-path
+ * @example
+ * Mac OSX
+ * ```js
+ * > getHomePath()
+ * '/Users/Lloyd'
+ * ```
+ *
+ * Ubuntu Linux
+ * ```js
+ * > getHomePath()
+ * '/home/lloyd'
+ * ```
+ *
+ * Windows 8.1
+ * ```js
+ * > getHomePath()
+ * 'C:\\Users\\Lloyd'
+ * ```
+ */
 function getHomePath () {
   if (process.platform === 'win32') {
     return process.env.USERPROFILE || process.env.HOMEDRIVE + process.env.HOMEPATH || process.env.HOME
@@ -40,3 +38,6 @@ function getHomePath () {
     return process.env.HOME
   }
 }
+
+var os = require('os')
+module.exports = os.homedir ? os.homedir : getHomePath
